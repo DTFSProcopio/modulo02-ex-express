6d496341aa2770e0a870ca13aa20589e13a77289
DTFSProcopio <danilo.procopio@mercadolivre.com>
2022-07-22 14:12:56 -0300

    primeira atividade

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
 const express = require("express");
 const app = express();
-app.use(express.json());
-const p = [];
-app.get("/", (req, res) => {
-  res.send("Hello World");
+app.use(express.json())
+app.get("/", (_req, res) => {
+  res.send("Olá mundo!");
 });
-app.get("/new", (req, res) => {
-  const newData = "ddd";
+app.get("/new", (_req, res) => {
+  const newData = "Olá novo mundo!";
   res.send(newData);
 });
-app.post("/", (req, res) => {
-  const { name } = req.body;
-  p.push(name);
-  res.send(p);
-});
-app.put("/", (req,res)=>{
-  const {index, name} = req.body;
-  if(index > p.length - 1){
-    return res.send("Index out of range!");
-  }
-  p[index] = name;
-  res.send(p);
-})
-app.listen(3000, () => console.log("Servidor em execução!"));
+
+app.listen(3001, () => console.log("Servidor em execução!"));
