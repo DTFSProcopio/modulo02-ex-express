2a951b6df5a007d73bc0a5a0d48487159ed02aeb
DTFSProcopio <danilo.procopio@mercadolivre.com>
2022-07-22 16:30:35 -0300

    exericio aula Express completo

--- app.js
+++ app.js
@@ -1,12 +1,58 @@
 const express = require("express");
+let products = require("./products");
 const app = express();
-app.use(express.json())
-app.get("/", (_req, res) => {
-  res.send("Olá mundo!");
+app.use(express.json());
+app.get("/products", (_req, res) => {
+  res.send(products);
 });
-app.get("/new", (_req, res) => {
-  const newData = "Olá novo mundo!";
-  res.send(newData);
+app.get("/product/:id", (req, res) => {
+  const id = Number(req.params.id);
+  product = products.find((product) => product.id === id);
+  res.send(product);
+});
+app.post("/product", (req, res) => {
+  const content = req.body;
+  if (content.length) {
+    content.forEach((product) => products.push(product));
+  } else {
+    products.push(content);
+  }
+  res.send(products);
+});
+app.put("/product/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const found = products.find((product) => product.id === id);
+  if (!found) {
+    return res.status(404).send({ message: "item não encontrado!" });
+  }
+  const content = req.body;
+  products[id - 1] = { ...content };
+  res.send(products);
+});
+app.patch("/product/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const found = products.find((product) => product.id === id);
+  if (!found) {
+    return res.status(404).send({ message: "item não encontrado!" });
+  }
+  const content = req.body;
+  products = products.map((product) => {
+    if (product.id === id) {
+      return { ...product, ...content };
+    }
+    return product;
+  });
+  res.send(products);
+});
+
+app.delete("/product/:id", (req, res) => {
+  const id = Number(req.params.id);
+  const found = products.find((product) => product.id === id);
+  if (!found) {
+    return res.status(404).send({ message: "item não encontrado!" });
+  }
+  products = products.filter((product) => product.id !== id);
+  res.sendStatus(204);
 });
 
 app.listen(3001, () => console.log("Servidor em execução!"));
